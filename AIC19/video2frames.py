2a59a3b67af33ca5a1b02bc7074d2690c4db48be
Yizhou Wang <ywang26@uw.edu>
2020-09-06 16:39:08 -0700

    test on ubuntu 18

---
+++ AIC19/video2frames.py
@@ -0,0 +1,43 @@
+import cv2
+import os
+
+split = 'test'
+data_root = '/mnt/disk1/AICity19/aic19-track1-mtmc'
+s_list = os.listdir(os.path.join(data_root, split))
+
+for s_id in s_list:
+    c_list = os.listdir(os.path.join(data_root, split, s_id))
+    for c_id in c_list:
+        # Read the video from specified path
+        video_path = os.path.join(data_root, split, s_id, c_id, 'vdo.avi')
+        cam = cv2.VideoCapture(video_path)
+
+        # creating a folder named data
+        images_path = os.path.join(data_root, split, s_id, c_id, 'images')
+        if not os.path.exists(images_path):
+            os.makedirs(images_path)
+
+        # frame
+        currentframe = 1
+
+        while True:
+            # reading from frame
+            ret, frame = cam.read()
+
+            if ret:
+                # if video is still left continue creating images
+                name = os.path.join(images_path, '%06d.jpg' % currentframe)
+                print('Creating...' + name)
+
+                # writing the extracted images
+                cv2.imwrite(name, frame)
+
+                # increasing counter so that it will
+                # show how many frames are created
+                currentframe += 1
+            else:
+                break
+
+    # Release all space and windows once done
+    # cam.release()
+    # cv2.destroyAllWindows()
